8168c61f8102bcbf1e2738a29b79001b2cd7bbad
Kavins <123402644+kavin2057@users.noreply.github.com>
2024-10-18 00:22:15 +0530

    Add files via upload

---
+++ ecom.js
@@ -0,0 +1,128 @@
+
+
+var sidenav = document.querySelector(".sidenav")
+
+function openNav() {
+    sidenav.style.right = "0%";
+}
+function closeNav() {
+    sidenav.style.right = "-100%";
+}
+
+
+var sliderleftbutton = document.getElementById("slider-left-activate")
+var sliderrightbutton =document.getElementById("slider-right-activate")
+var sliderimage= document.querySelector(".slider-image-container")
+var slidermargin=0
+
+console.log(sliderleftbutton)
+
+sliderrightbutton.addEventListener("click",
+
+function(){
+    slidermargin=slidermargin+100
+
+    if(slidermargin>200)
+    {
+        slidermargin=0
+        sliderimage.style.marginLeft=0;
+    }
+    else{
+        sliderimage.style.marginLeft="-"+slidermargin+"vw";
+    }
+    
+   
+
+}
+)
+
+sliderleftbutton.addEventListener("click",
+
+function(){
+
+    if(slidermargin==0)
+    {
+       
+        slidermargin=200
+        sliderimage.style.marginLeft="-"+slidermargin+"vw";
+    }
+    else{
+
+        slidermargin=slidermargin-100
+    sliderimage.style.marginLeft="-"+slidermargin+"vw";
+    }
+    
+    
+}
+)
+
+const initialScrollElements = document.querySelectorAll('.initial-scroll-animate');
+const revealScrollElements = document.querySelectorAll('.reveal-scroll-animate');
+
+const options = {
+  root: null, 
+  rootMargin: '0px',
+  threshold: 0.2,
+};
+
+const initialScrollObserver = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('active');
+      initialScrollObserver.unobserve(entry.target); 
+    }
+  });
+}, options);
+
+const revealScrollObserver = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('active');
+      revealScrollObserver.unobserve(entry.target); 
+    }
+  });
+}, options);
+
+initialScrollElements.forEach((element) => {
+  initialScrollObserver.observe(element);
+});
+
+revealScrollElements.forEach((element) => {
+  revealScrollObserver.observe(element);
+});
+
+var likebuttons=document.querySelectorAll(".like-button")
+
+likebuttons.forEach((btn)=>{
+    btn.addEventListener("click",function(e){
+        
+        
+        if(e.target.src.indexOf("blackheart")>0)
+        {
+            console.log("jkjk");
+            e.target.src="img/icons/redheart.png"
+        }
+        else{
+            e.target.src="img/icons/blackheart.png"
+        }
+    })
+})
+
+window.addEventListener("scroll",function(){
+    var elements = this.document.querySelectorAll(".initial-scroll-animate")
+    elements.forEach((el)=>{
+        windowHeight = window.innerHeight
+     var elbound = el.getBoundingClientRect()
+   
+
+     console.log(windowHeight)
+     console.log(elbound.top)
+     if(windowHeight>elbound.top-100){
+        console.log("Hi")
+    el.classList.remove("reveal-scroll-animate")
+
+    }
+    
+})
+    
+})
